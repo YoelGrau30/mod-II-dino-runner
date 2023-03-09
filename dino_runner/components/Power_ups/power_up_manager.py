b8377085dfb5b77615bfce4b44b28aa3685364a8
YoelGrau30 <y.yoelgrau07@gmail.com>
2023-03-09 15:17:33 -0500

    add

--- dino_runner/components/Power_ups/power_up_manager.py
+++ dino_runner/components/Power_ups/power_up_manager.py
@@ -1,15 +1,22 @@
 from dino_runner.components.power_ups.shield import Shield
 from dino_runner.utils.constants import SHIELD
+import random
 
 
 class PowerUpManager:
 
+    POWER_UP_PROBABILITY = 2
+
     def __init__(self):
         self.power_ups = []
+
+    def generate_power_up(self):
+        if random.randint(0, 100) < self.POWER_UP_PROBABILITY:
+            self.power_ups.append(Shield(SHIELD))
         
     def update(self, game):
         if len(self.power_ups) == 0:
-            self.power_ups.append(Shield(SHIELD))
+            self.generate_power_up()
         
         for power_up in self.power_ups:
             power_up.update(game.game_speed, self.power_ups)
